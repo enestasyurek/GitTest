bcab3ecc5430b80fcc674dc63dca34ef5ade7336
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-11-10 00:32:06 +0300

    test pr

---
+++ src/test/java/Colonels/pages/loginPage.java
@@ -0,0 +1,8 @@
+package Colonels.pages;
+
+public class loginPage {
+    public static void main(String[] args) {
+        System.out.println("Hello !");
+        System.out.println("Test !");
+    }
+}
