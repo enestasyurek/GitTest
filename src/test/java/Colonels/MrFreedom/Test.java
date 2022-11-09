ef2e155ac340db4a8badd80d6b858d67b0da6644
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-11-09 23:17:16 +0300

    Initial Commit

---
+++ src/test/java/Colonels/MrFreedom/Test.java
@@ -0,0 +1,7 @@
+package Colonels.MrFreedom;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Sir!");
+    }
+}
---
+++ src/test/java/Colonels/MrSmile/Test.java
@@ -0,0 +1,7 @@
+package Colonels.MrSmile;
+
+public class Test {
+public static void main(String[] args) {
+        System.out.println("Hello Sir!");
+    }
+}
