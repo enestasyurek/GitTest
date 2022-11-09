f8fae5e611c48a1ddc383914104c51b96db2f83a
veli-aydin <veli-aydin@hotmail.com>
2022-11-10 00:18:20 +0300

    update2

---
+++ src/test/java/Colonels/pages/loginPage.java
@@ -0,0 +1,9 @@
+package Colonels.pages;
+
+public class loginPage {
+
+    public static void main(String[] args) {
+        System.out.println("hi guys");
+        System.out.println("this is Veli");
+    }
+}
