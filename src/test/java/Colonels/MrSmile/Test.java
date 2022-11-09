77ed181b031d0c241880e08482c527d0a77d31e0
Enes Taşyürek <tasyurekenes22@gmail.com>
2022-11-09 23:34:13 +0300

    Initial Commit

--- src/test/java/Colonels/MrSmile/Test.java
+++ src/test/java/Colonels/MrSmile/Test.java
@@ -3,5 +3,6 @@ package Colonels.MrSmile;
 public class Test {
 public static void main(String[] args) {
         System.out.println("Hello Sir!");
+        System.out.println("Hello Sir2!");
     }
 }
